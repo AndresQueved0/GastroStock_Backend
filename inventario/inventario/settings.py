a35d293397ae208594e3bac42b797337b26181cd
AndresQueved0 <quevedo0504@gmail.com>
2024-09-04 18:12:16 -0500

    server

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin_panel.apps.AdminPanelConfig',
+    'admin_panel.apps.AdminPanelConfig'
+    'admin_panel',
 ]
 
 MIDDLEWARE = [
